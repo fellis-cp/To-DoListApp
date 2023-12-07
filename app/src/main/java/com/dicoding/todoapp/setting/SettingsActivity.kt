e4c38d7376e81bae31764837fc23e4a56936ed45
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-08 00:40:35 +0700

    TODO 13
     //TODO 13 : Schedule and cancel daily reminder using WorkManager with data channelName

--- app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
+++ app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.dicoding.todoapp.setting
 
 import android.Manifest
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -8,7 +9,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.dicoding.todoapp.R
+import com.dicoding.todoapp.notification.NotificationWorker
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -49,9 +54,34 @@ class SettingsActivity : AppCompatActivity() {
             prefNotification?.setOnPreferenceChangeListener { preference, newValue ->
                 val channelName = getString(R.string.notify_channel_name)
                 //TODO 13 : Schedule and cancel daily reminder using WorkManager with data channelName
+                if(newValue == true ) {
+                    setReminder(channelName)
+                } else {
+                    cancelReminder()
+                }
+
                 true
             }
 
         }
+
+        private fun setReminder (channelName: String){
+            val workManager = WorkManager.getInstance(requireContext())
+            val workRequest = OneTimeWorkRequestBuilder<NotificationWorker>()
+                .addTag(WORKER_TAG)
+                .build()
+            workManager.enqueueUniqueWork(channelName, ExistingWorkPolicy.REPLACE, workRequest)
+        }
+
+        private fun cancelReminder () {
+            val workManager = WorkManager.getInstance(requireContext())
+            workManager.cancelUniqueWork(WORKER_NAME)
+        }
     }
+
+    companion object {
+        const val WORKER_NAME = "h-1"
+        const val WORKER_TAG = "daily_notif"
+    }
+
 }
