8d3ecb1087c2df384a3c0251183a9915e1e7bcfe
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-08 02:11:57 +0700

    fixing bug

--- app/src/main/java/com/dicoding/todoapp/data/TaskDao.kt
+++ app/src/main/java/com/dicoding/todoapp/data/TaskDao.kt
@@ -10,29 +10,32 @@ import androidx.room.Query
 import androidx.room.RawQuery
 import androidx.sqlite.db.SupportSQLiteQuery
 
+
 //TODO 2 : Define data access object (DAO)
 @Dao
 interface TaskDao {
 
-    @RawQuery(observedEntities = [Task :: class])
-    fun getTasks(query: SupportSQLiteQuery): DataSource.Factory<Int, Task>
+    @RawQuery(observedEntities = [Task::class])
+    fun getTasks(query: SupportSQLiteQuery): DataSource.Factory <Int, Task>
 
-    @Query("SELECT * FROM task WHERE id= :taskId")
+    @Query("SELECT * FROM tasks WHERE id=:taskId")
     fun getTaskById(taskId: Int): LiveData<Task>
 
-    @Query("SELECT * FROM task WHERE completed = 0 ORDER BY dueDate ASC LIMIT 1 ")
+    @Query("SELECT * FROM tasks WHERE completed = 0 ORDER BY dueDate ASC LIMIT 1")
     fun getNearestActiveTask(): Task
 
     @Insert
     suspend fun insertTask(task: Task): Long
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE )
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg tasks: Task)
 
     @Delete
     suspend fun deleteTask(task: Task)
 
-    @Query("UPDATE task SET completed=:completed WHERE id=:taskId")
+    @Query("UPDATE tasks SET completed=:completed WHERE id=:taskId")
     suspend fun updateCompleted(taskId: Int, completed: Boolean)
-    
+
+
+
 }
