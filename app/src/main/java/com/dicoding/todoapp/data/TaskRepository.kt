9351f7d88f8861e4a2910625a8373980f873a96e
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-07 22:22:19 +0700

    //TODO 4 : Use FilterUtils.getFilteredQuery to create filterable query     //TODO 5 : Build PagedList with configuration

--- app/src/main/java/com/dicoding/todoapp/data/TaskRepository.kt
+++ app/src/main/java/com/dicoding/todoapp/data/TaskRepository.kt
@@ -2,7 +2,9 @@ package com.dicoding.todoapp.data
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
+import com.dicoding.todoapp.utils.FilterUtils
 import com.dicoding.todoapp.utils.TasksFilterType
 
 class TaskRepository(private val tasksDao: TaskDao) {
@@ -29,7 +31,13 @@ class TaskRepository(private val tasksDao: TaskDao) {
     //TODO 4 : Use FilterUtils.getFilteredQuery to create filterable query
     //TODO 5 : Build PagedList with configuration
     fun getTasks(filter: TasksFilterType): LiveData<PagedList<Task>> {
-        throw NotImplementedError("Not yet implemented")
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(PLACEHOLDERS)
+            .setPageSize(PAGE_SIZE)
+            .build()
+        val query = FilterUtils.getFilteredQuery(filter)
+        val tasks = tasksDao.getTasks(query)
+        return LivePagedListBuilder(tasks , config).build()
     }
 
     fun getTaskById(taskId: Int): LiveData<Task> {
