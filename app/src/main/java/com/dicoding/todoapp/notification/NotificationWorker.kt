7cf14f65880416e41125aa702924f14b7fc3eb80
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-08 01:12:39 +0700

    TODO 14
    //TODO 14 : If notification preference on, get nearest active task from repository and show notification with pending intent

--- app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
@@ -1,14 +1,23 @@
 package com.dicoding.todoapp.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.TaskStackBuilder
+import androidx.preference.PreferenceManager
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.dicoding.todoapp.R
 import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.data.TaskRepository
 import com.dicoding.todoapp.ui.detail.DetailTaskActivity
+import com.dicoding.todoapp.utils.DateConverter
 import com.dicoding.todoapp.utils.NOTIFICATION_CHANNEL_ID
 import com.dicoding.todoapp.utils.TASK_ID
 
@@ -32,7 +41,45 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
 
     override fun doWork(): Result {
         //TODO 14 : If notification preference on, get nearest active task from repository and show notification with pending intent
+        val preference = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val preferenceNotification = preference.getBoolean(applicationContext.getString(R.string.pref_key_notify), false)
+
+        if (preferenceNotification) {
+            val nearestTask = TaskRepository.getInstance(context = applicationContext).getNearestActiveTask()
+            getPendingIntent(nearestTask)?.let {
+                showNotif(applicationContext, nearestTask, it)
+            }
+            Log.d("Nearest Task", nearestTask.title)
+        }
         return Result.success()
     }
 
+    private fun showNotif(context: Context, task: Task, pendingIntent: PendingIntent) {
+        val channelId = NOTIFICATION_CHANNEL_ID
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val existingChannel = (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .getNotificationChannel(channelId)
+
+            if (existingChannel == null) {
+                val myChannel = NotificationChannel(channelId, "Channel Name", NotificationManager.IMPORTANCE_DEFAULT)
+                myChannel.description = "Task Reminder"
+
+                val notifManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notifManager.createNotificationChannel(myChannel)
+            }
+        }
+
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(task.title)
+            .setContentIntent(pendingIntent)
+            .setContentText(context.getString(R.string.notify_content, DateConverter.convertMillisToString(task.dueDateMillis)))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val notificationId = task.id
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(notificationId, builder.build())
+        }
+    }
 }
