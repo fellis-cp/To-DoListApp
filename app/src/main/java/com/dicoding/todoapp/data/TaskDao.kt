0622a7762f7eb3cb0bbe70105c976cf76d701921
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-08 00:01:33 +0700

    Fix Error

--- app/src/main/java/com/dicoding/todoapp/data/TaskDao.kt
+++ app/src/main/java/com/dicoding/todoapp/data/TaskDao.kt
@@ -2,6 +2,7 @@ package com.dicoding.todoapp.data
 
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -10,12 +11,13 @@ import androidx.room.RawQuery
 import androidx.sqlite.db.SupportSQLiteQuery
 
 //TODO 2 : Define data access object (DAO)
+@Dao
 interface TaskDao {
 
     @RawQuery(observedEntities = [Task :: class])
     fun getTasks(query: SupportSQLiteQuery): DataSource.Factory<Int, Task>
 
-    @Query("SELECT * FROM tasks WHERE id= :taskId")
+    @Query("SELECT * FROM task WHERE id= :taskId")
     fun getTaskById(taskId: Int): LiveData<Task>
 
     @Query("SELECT * FROM task WHERE completed = 0 ORDER BY dueDate ASC LIMIT 1 ")
